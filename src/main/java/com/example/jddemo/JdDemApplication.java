565f5f7c1b6d38ef14082b7501e982bdd04d83be
ext.zhaolupeng <1>
2020-10-09 10:37:12 +0800

    1

--- src/main/java/com/example/jddemo/JdDemApplication.java
+++ src/main/java/com/example/jddemo/JdDemApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JdDemApplication {
     public static void main(String[] args) throws Exception {
-
         SpringApplication.run(JdDemApplication.class, args);
     }
 
