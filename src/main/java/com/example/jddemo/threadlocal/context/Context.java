aa3b0d03e40e55c09fdd6e06d7431e3f45c00d26
ext.zhaolupeng <1>
2020-10-22 16:10:36 +0800

    111

--- src/main/java/com/example/jddemo/threadlocal/context/Context.java
+++ src/main/java/com/example/jddemo/threadlocal/context/Context.java
@@ -5,11 +5,8 @@ public class Context {
     String  str="单个文件回滚";
     private static final AbstractContextCore contextCore=new ContextCore();
 
-
     public static String getXID() {
         return contextCore.get("Tcc");
     }
-
-
 
 }
