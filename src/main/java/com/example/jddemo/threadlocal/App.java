e27b54c86d0ffe6afc475a80f1583cb19e3c06df
ext.zhaolupeng <1>
2020-10-22 15:49:36 +0800

    11

--- src/main/java/com/example/jddemo/threadlocal/App.java
+++ src/main/java/com/example/jddemo/threadlocal/App.java
@@ -6,17 +6,12 @@ public class App {
 
 
     public static void main(String[] args) {
-
         for (int i = 0; i < 2; i++) {//启动三个线程
             Thread t = new Thread() {
                 @Override
                 public void run() {
                     String tcc = Context.getXID();
                     System.out.println(Thread.currentThread().getName()+"="+tcc);
-
-
-
-
                     new Thread(()->{
                         String  tcc1 = Context.getXID();
                         System.out.println(Thread.currentThread().getName()+"="+tcc1);
