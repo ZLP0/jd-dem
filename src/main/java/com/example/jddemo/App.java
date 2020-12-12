fd7167884897e0829e316dfb7bfe9407f047d9af
ZLP00 <1186121106@qq.com>
2020-12-12 15:47:09 +0800

    jacksonutils 工具类

---
+++ src/main/java/com/example/jddemo/App.java
@@ -0,0 +1,30 @@
+package com.example.jddemo;
+
+import com.example.jddemo.copy.Person;
+import com.example.jddemo.jackson.JacksonUtils;
+
+/**
+ * 程序员  by dell
+ * time  2020-12-09
+ **/
+
+public class App {
+
+
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        person.setName("张三");
+        person.setAge(13);
+        String s = JacksonUtils.toJson(person);
+        System.out.println(s);
+
+        Person person1 = JacksonUtils.fromIgnoreJson(s, Person.class);
+
+        System.out.println(person1);
+        JacksonUtils.
+
+    }
+
+
+}
