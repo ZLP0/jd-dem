d66ae75266c7ab1d4e4e7470a52a1f865a8d2d3e
ext.zhaolupeng <1>
2020-12-31 15:42:32 +0800

    删除

--- src/main/java/com/example/jddemo/App.java
+++ src/main/java/com/example/jddemo/App.java
@@ -1,18 +1,7 @@
 package com.example.jddemo;
 
-import com.alibaba.excel.EasyExcel;
-import com.example.jddemo.copy.Person;
-import com.example.jddemo.excel.upload.excellistener.AbstractEasyExcelDataListener;
-import com.example.jddemo.excel.upload.excellistener.ExcelDto;
-import com.example.jddemo.excel.upload.excellistener.MyDataEasyExcelListener;
-import com.example.jddemo.jackson.JacksonUtils;
-import com.example.jddemo.response.ResponseEntity;
-import org.springframework.beans.factory.ObjectFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.annotation.Resource;
-import java.util.AbstractList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 程序员  by dell
@@ -20,8 +9,10 @@ import java.util.AbstractList;
  **/
 public class App {
 
-//第一次提交
+    Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
+
 
     }
 
--- src/main/java/com/example/jddemo/extend/App.java
+++
@@ -1,6 +0,0 @@
-package com.example.jddemo.extend;
-
-import com.example.jddemo.manager.Animal;
-
-public class App<T extends Animal> {
-}
