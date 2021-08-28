4f948333866583d75ced60c99909e35c46efb6e7
ZLP00 <1186121106@qq.com>
2021-08-28 18:27:19 +0800

    NIO  多线程 reactor 模型

--- src/main/java/com/example/jddemo/nio/reactorthread/MultiReactorMain.java
+++ src/main/java/com/example/jddemo/nio/reactorthread/MultiReactorMain.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class MultiReactorMain {
     public static void main(String[] args) {
         try {
-            NIOReactorServerSocket nioReactorServerSocket = new NIOReactorServerSocket(8080);
+            NIOMultiReactorServerSocket nioReactorServerSocket = new NIOMultiReactorServerSocket(8080);
             new Thread(nioReactorServerSocket).start();
         } catch (IOException e) {
             e.printStackTrace();
